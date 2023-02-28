2e7506df570430f4f19670aeacda08d1683e05dc
Heng Tse Chou <legohank1219@gmail.com>
2023-02-28 17:13:19 +0800

    map search object created

---
+++ bot.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, Request, HTTPException
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from dotenv import load_dotenv
+import os
+import logging
+
+from map import Search_map
+
+logger = logging.getLogger("uvicorn")
+
+
+load_dotenv()
+app = FastAPI()
+
+# Line Bot config
+accessToken = os.getenv('CHANNEL_ACCESS_TOKEN')
+secret = os.getenv('CHANNEL_SECRET')
+
+line_bot_api = LineBotApi(accessToken)
+handler = WebhookHandler(secret)
+
+@app.post("/")
+async def Bot(request: Request):
+    signature = request.headers["X-Line-Signature"]
+    body = await request.body()
+    # logger.info(body.decode())
+    try:
+        handler.handle(body.decode(), signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="Missing Parameters")
+    return "OK"
+
+
+
+@handler.add(MessageEvent)
+def handling_message(event):
+    replyToken = event.reply_token
+    msg = event.message
+    msg_type = event.message.type
+
+    result = msg_type
+
+    if msg_type == 'location':
+        search = Search_map()
+        search.set_coordinates(msg.longitude, msg.latitude)
+        search.set_radius(1500)
+        search.set_type('restaurant')
+
+        result = search.get_result()
+        logger.info(result)
+        logger.info(len(result))
+
+
+    
+
+    echoMessages = TextSendMessage(text=result)
+    line_bot_api.reply_message(reply_token=replyToken, messages=echoMessages)
+
+# uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ map.py
@@ -0,0 +1,46 @@
+import requests
+import os 
+import logging
+from dotenv import load_dotenv
+load_dotenv()
+
+
+logger = logging.getLogger("uvicorn")
+
+
+API_KEY = os.getenv('MAP_API')
+logger.info(API_KEY)
+
+class Search_map(object):
+
+    def __init__(self):
+        self.api_key = str(API_KEY)
+    def __set_longitude(self, longnitude):
+        self.longnitude = str(longnitude)
+    def __set_latitude(self, latitude):
+        self.latitude = str(latitude)
+
+    def set_coordinates(self, longnitude, latitude):
+        self.__set_longitude(longnitude)
+        self.__set_latitude(latitude)
+
+    def set_radius(self, radius):
+        self.radius = str(radius)
+    
+    def set_type(self, location_type):
+        self.type = str(location_type)
+
+    def get_result(self):
+
+        url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={}%2C{}&radius={}&type={}&key={}&opennow=true".format(
+            self.latitude, self.longnitude, self.radius, self.type, self.api_key
+        )
+
+        logger.info(url)
+
+        # payload={}
+        # headers = {}
+
+        response = requests.request("GET", url)
+
+        return response.text
