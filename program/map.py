a68c058089d01dbbfc724e57af18eb1afbc278bc
Heng Tse Chou <legohank1219@gmail.com>
2023-03-13 23:17:28 +0800

    chore: set attrs in Search object as none when initialize

--- program/map.py
+++ program/map.py
@@ -10,6 +10,10 @@ class Search_map(object):
 
     def __init__(self):
         self.api_key = MAP_API_KEY
+        self.latitude = None
+        self.longitude = None
+        self.keyword = None
+        self.radius = None
         
     def __set_latitude(self, latitude):
         self.latitude = str(latitude)
@@ -18,8 +22,7 @@ class Search_map(object):
 
     def set_coordinates(self, latitude, longnitude):
         self.__set_latitude(latitude)
-        self.__set_longitude(longnitude)
-        
+        self.__set_longitude(longnitude)        
 
     def set_radius(self, radius):
         self.radius = str(radius)
@@ -29,8 +32,7 @@ class Search_map(object):
 
     def get_result(self):
 
-        attrs = ['latitude', 'longitude', 'keyword', 'radius']
-        if all(hasattr(self, attr) for attr in attrs):
+        if None not in [self.latitude, self.longitude, self.keyword, self.radius]:
 
             url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={}%2C{}&radius={}&keyword={}&key={}&opennow=true&language=zh-TW".format(
                 self.latitude, self.longitude, self.radius, self.keyword, self.api_key
